b8c80c6eecf8297c67faafc1d14a72e37dd3d636
Yossi Tamari <Yossi.Tamari@quartech.com>
2020-07-16 11:01:40 -0700

    ES based message bus

---
+++ ess/src/EMBC.ESS/domain/common/Domain_ESBus.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using EventStore.ClientAPI;
+
+namespace EMBC.ESS.Domain.Common
+{
+    public class Domain_ESBus : IBus
+    {
+        private readonly IEventStoreConnection conn;
+
+        public Domain_ESBus(IEventStoreConnection conn)
+        {
+            this.conn = conn;
+            var handlers = Assembly.GetExecutingAssembly().ExportedTypes.Where(t =>
+            t.GetMethods().Any(m => m.IsPublic && m.Name.StartsWith("Handle") && m.GetParameters().Length == 1 && typeof(Event).IsAssignableFrom(m.GetParameters()[0].ParameterType)));
+        }
+
+        public async Task PublishAsync<T>(T evt) where T : Event
+        {
+            await Task.CompletedTask;
+        }
+
+        public async Task SendAsync(ICommand command)
+        {
+            await Task.CompletedTask;
+        }
+
+        public async Task<TResponse> SendAsync<TResponse>(IRequest<TResponse> command)
+        {
+            await Task.CompletedTask;
+            throw new NotImplementedException();
+        }
+    }
+}
