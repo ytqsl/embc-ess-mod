aa87797621d67a465ff3547121dfcd89714902bc
Yossi Tamari <Yossi.Tamari@quartech.com>
2020-09-03 10:54:26 -0700

    dynamics sync code

---
+++ ess/src/EMBC.DynamicsSynchronizer/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using EventStore.Client;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Debugging;
+
+namespace EMBC.DynamicsSynchronizer
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            SelfLog.Enable(Console.Error);
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            var builder = new HostBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    var settings = new EventStoreClientSettings
+                    {
+                        OperationOptions = new EventStoreClientOperationOptions { ThrowOnAppendFailure = true },
+                        CreateHttpMessageHandler = () => new SocketsHttpHandler
+                        {
+                            SslOptions = { RemoteCertificateValidationCallback = (sender, certificate, chain, errors) => true }
+                        },
+                        DefaultCredentials = new UserCredentials("admin", "changeit"),
+                        ConnectivitySettings = new EventStoreClientConnectivitySettings()
+                        {
+                            Address = new Uri("https://localhost:2113")
+                        },
+                        ConnectionName = "EMBC.ESS",
+                    };
+                    var conn = new EventStoreClient(settings);
+                    services.AddSingleton(conn);
+                    services.AddLogging();
+                    services.AddHostedService<SynchronizerService>();
+                })
+                .UseSerilog();
+
+            await builder.RunConsoleAsync();
+        }
+    }
+}
---
+++ ess/src/EMBC.DynamicsSynchronizer/SynchronizerService.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using EventStore.Client;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace EMBC.DynamicsSynchronizer
+{
+    public class SynchronizerService : IHostedService, IDisposable
+    {
+        private bool disposedValue;
+        private StreamSubscription subscription;
+        private readonly EventStoreClient conn;
+        private readonly ILogger logger;
+
+        public SynchronizerService(EventStoreClient conn, ILogger<SynchronizerService> logger)
+        {
+            this.conn = conn;
+            this.logger = logger;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            subscription = await conn.SubscribeToAllAsync(Position.Start,
+                 async (sub, evt, _) =>
+                {
+                    await Task.CompletedTask;
+                    logger.LogDebug("Sending {0} to Dynamics: {1}", evt.Event.EventType, Encoding.UTF8.GetString(evt.Event.Data.Span.ToArray()));
+                }, true,
+                (sub, reason, ex) =>
+                {
+                    logger.LogError(ex, "subscription dropped: {0}", reason);
+                },
+                new SubscriptionFilterOptions(EventTypeFilter.ExcludeSystemEvents()));
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            await Task.CompletedTask;
+            subscription.Dispose();
+            subscription = null;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    if (subscription != null) subscription.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
